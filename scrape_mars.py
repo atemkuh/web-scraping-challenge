6b0117f7b6ceda466db3ee9629f1761a87c19928
Joseph Atemkuh <atemkuh@icloud.com>
2020-11-27 11:58:50 -0500

    scrape_mars.py .... #Link to Mars image to be scraped

--- scrape_mars.py
+++ scrape_mars.py
@@ -10,13 +10,20 @@ def init_browser():
     executable_path = {'executable_path': ChromeDriverManager().install()}
     browser = Browser('chrome', **executable_path, headless=False)
 
-def scrape():
-    Browser = init_browser()
+ddef scrape():
+    browser = init_browser()
     mars_dict ={}
 
-    #URL of Mars data to be scraped
-    news_url = 'https://mars.nasa.gov/news/'
-    Browser.visit(news_url)
+    # Mars News URL of page to be scraped
+    source_news_url = 'https://mars.nasa.gov/news/'
+    browser.visit(news_url)
     html = browser.html
     soup = bs(html, 'html.parser')
-    
+    # Retrieve the latest news title and paragraph
+    latest_news_title = news_soup.find_all('div', class_='content_title')[0].text
+    latest_news_paragraph = soup.find_all('div', class_='article_teaser_body')[0].text
+    #Link to Mars image to be scraped
+    nasa_jpl_url ='https://www.jpl.nasa.gov/'
+    nasa_jpl_images_url = 'https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars'
+    browser.visit(nasa_jpl_images_url)
+    soup_images = bs(html,'html.parser')
